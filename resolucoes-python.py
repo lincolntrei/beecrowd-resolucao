3adab03749a9ad91f0132baa7489d2c8d2b0c382
Lincoln Ferreira <lincolnferreira2012@gmail.com>
2025-05-01 21:34:28 -0300

    Problemas 1000 ao 1004
    Problemas simples de manipulação de variáveis e exibir informações.

---
+++ resolucoes-python.py
@@ -0,0 +1,26 @@
+#Problema 1000 - Hello World
+print("Hello World!")
+
+#Problema 1001 - Extremely Basic
+A = int(input())
+B = int(input())
+X = A + B
+print("X = " + str(X))
+
+#Problema 1002 - Área of the Circle
+n = 3.14159
+raio = float(input())
+area = n * (raio * raio)
+print(f"A={area:.4f}") #limitar tamaho das casas decimais
+
+#Problema 1003 - Simple Sum
+A = int(input())
+B = int(input())
+SOMA = A + B
+print(f"SOMA = {SOMA}")
+
+#Problema 1004 - Simple Product
+A = int(input())
+B = int(input())
+PROD = A * B
+print("PROD =", PROD)
