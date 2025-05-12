15d977d763b554f002755ad45f057cab36847170
Lincoln Ferreira <lincolnferreira2012@gmail.com>
2025-05-12 16:23:38 -0300

    Adicionado Resolução 2605 e 2606
    Selects com JOIN filtrando por informações de outras tabelas.

--- resolucoes-sql.sql
+++ resolucoes-sql.sql
@@ -6,3 +6,14 @@ SELECT name, street FROM customers WHERE city = 'Porto Alegre';
 
 -- Problema 2604 - Under 10 or Greater Than 100
 SELECT id, name FROM products WHERE price < 10 OR price > 100;
+
+-- Problema 2605 - Executive Representatives
+SELECT p.name AS name, f.name AS name FROM products p
+JOIN providers f ON p.id_providers = f.id
+JOIN categories c ON p.id_categories = c.id
+WHERE c.name = 'executive';
+
+-- Problema 2606 - Categories
+SELECT p.id AS id, p.name AS name FROM products p
+JOIN categories c ON p.id_categories = c.id
+WHERE c.name ILIKE 'super%';
